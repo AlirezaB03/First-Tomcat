5061efcbf7fb8a901cc2e28d86fef03b151d9047
amir <amirbahraami99@gmail.com>
2024-08-08 17:31:04 +0330

    add a new java file Test_servlet

--- src/main/java/test/Test_servlet.java
+++ src/main/java/test/Test_servlet.java
@@ -1,4 +1,29 @@
 package test;
 
-public class Test_servlet {
-}
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "Test_servlet", value = "/Test_servlet")
+public class Test_servlet extends HttpServlet {
+        private String message;
+
+        public void init() {
+            message = "Hello world in test!";
+        }
+
+        public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.setContentType("text/html");
+
+            // Hello
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1>" + message + "</h1>");
+            out.println("</body></html>");
+        }
+
+        public void destroy() {
+        }
+    }
+
